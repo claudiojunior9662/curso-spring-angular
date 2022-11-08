2f84f18e13f3b0c5fa37c223abc3f74bc324071c
Claudio Júnior <claudiojunior9662@gmail.com>
2022-11-08 20:30:51 -0300

    [ADD] aula 57

--- src/clientes-app/src/app/app.module.ts
+++ src/clientes-app/src/app/app.module.ts
@@ -6,6 +6,8 @@ import { AppComponent } from './app.component';
 
 import { TemplateModule } from './template/template.module';
 import { HomeComponent } from './home/home.component';
+import { ClientesModule } from './clientes/clientes.module';
+import { RouterModule } from '@angular/router';
 
 @NgModule({
   declarations: [
@@ -15,7 +17,9 @@ import { HomeComponent } from './home/home.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    TemplateModule
+    TemplateModule,
+    ClientesModule,
+    RouterModule
   ],
   providers: [],
   bootstrap: [AppComponent]
